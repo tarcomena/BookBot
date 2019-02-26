5a6ff4f9f540218437dbd27cf1462fdf82c93d77
Tena Salais, Marco A <marco-tenasalais@uowa.edu>
2019-02-25 22:28:05 -0600

    starting
    Connection started with Javamail

--- src/EmailConnection.java
+++ src/EmailConnection.java
@@ -1,4 +1,5 @@
 import java.util.Properties;
+import java.io.*;
 
 import javax.mail.*;
 
@@ -8,7 +9,7 @@ public class EmailConnection {
 
     private String password;
 
-    private Properties connectionProperies;
+    private Properties properties;
 
     private String port;
 
@@ -16,49 +17,43 @@ public class EmailConnection {
 
 
 
-    public EmailConnection(String email, String password,String port ){
+    public EmailConnection(String email, String password){
         this.email=email;
         this.password=password;
         this.port=port;
-        connectionProperies=System.getProperties();
-        connectionProperies.setProperty("mail.store.protocol", "imaps");
-        connectionProperies.put("mail.imap-mail.outlook.com.ssl.enable", "true");
-        connectionProperies.put("mail.pop3.host", "outlook.com");
-        connectionProperies.put("mail.pop3.port", port);
-        connectionProperies.put("mail.pop3.starttls.enable", "true");
+        properties=System.getProperties();
+        properties.put("mail.store.protocol", "pop3");
+        properties.put("mail.pop3.host", "pop.gmail.com");
+        properties.put("mail.pop3.port", "995");
+        properties.put("mail.pop3.starttls.enable", "true");
+
         try {
-            Session session = Session.getInstance(connectionProperies, null);
+            Session session = Session.getInstance(properties, null);
+
             Store store = session.getStore();
-            store.connect("imap-mail.outlook.com", "myEmail@live.dk", "MyPassword");
+
+            store.connect("pop.gmail.com", email, password);
+
             session.setDebug(true);
+
             Folder inbox = store.getFolder("INBOX");
+
             inbox.open(Folder.READ_ONLY);
 
 
-            SearchTerm sender = new FromTerm(new InternetAddress("myMail@live.dk"));
+            store = session.getStore("pop3s");
 
-            Message[] messages = inbox.search(sender);
-            System.out.println(messages);
 
-            for (int i = 0 ; i < messages.length ; i++) {
 
-                System.out.println(messages[i].getSubject());
-                if (messages[i].getSubject().equals(null)) {
-                    System.out.println("null in subject");
-                    break;
-                }
-                else if (messages[i].getSubject().contains("Ordretest")){
-                    System.out.println("1 match found");
-                }
-                else {
-                    System.out.println("no match");
-                }
-            }
-            System.out.println("no more messages");
-            store.close();
 
-        } catch (Exception mex) {
-            mex.printStackTrace();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            // retrieve the messages from the folder in an array and print it
+            Message[] messages = inbox.getMessages();
+            System.out.println("messages.length---" + messages.length);
+
+        } catch (Exception e) {
+            System.out.println("no connection");
         }
     }
 
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String emailuserName="tarconator@gmail.com";
+        String password="Halo12344";
+
+        EmailConnection test = new EmailConnection(emailuserName,password);
+    }
+}
