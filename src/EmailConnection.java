d9a15cebcf88d9766135d72b94f4d9e3949c9e04
Tena Salais, Marco A <marco-tenasalais@uowa.edu>
2019-02-26 23:28:00 -0600

    Reads in Email Data
    Emails ofa gmail can be read in and scraped for data

--- src/EmailConnection.java
+++ src/EmailConnection.java
@@ -1,60 +1,120 @@
+import java.util.ArrayList;
 import java.util.Properties;
 import java.io.*;
 
 import javax.mail.*;
+import javax.mail.internet.MimeMultipart;
 
 public class EmailConnection {
 
     private  String email;
 
-    private String password;
+
 
     private Properties properties;
 
     private String port;
 
+    private ArrayList<ArrayList<String>> bookAndInfo;
 
 
-
+    /**Basic connection for Gmail
+     *
+     * @param email
+     * @param password
+     */
 
     public EmailConnection(String email, String password){
         this.email=email;
-        this.password=password;
-        this.port=port;
+        bookAndInfo=new ArrayList<>();
+
+
         properties=System.getProperties();
         properties.put("mail.store.protocol", "pop3");
         properties.put("mail.pop3.host", "pop.gmail.com");
         properties.put("mail.pop3.port", "995");
         properties.put("mail.pop3.starttls.enable", "true");
-
+        //
         try {
-            Session session = Session.getInstance(properties, null);
+            Session emailSession = Session.getDefaultInstance(properties);
 
-            Store store = session.getStore();
 
+            // create the POP3 store object and connect with the pop server
+            Store store = emailSession.getStore("pop3s");
+            //connection is started
+            // little if any security :(
             store.connect("pop.gmail.com", email, password);
 
-            session.setDebug(true);
 
             Folder inbox = store.getFolder("INBOX");
 
             inbox.open(Folder.READ_ONLY);
 
 
-            store = session.getStore("pop3s");
 
 
 
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
             // retrieve the messages from the folder in an array and print it
             Message[] messages = inbox.getMessages();
             System.out.println("messages.length---" + messages.length);
+            for(int i =messages.length-1;i>messages.length-messages.length+95;i--){
+               // System.out.println(messages[i].getContent().toString());
+
+                if(messages[i].getSubject().contains("library checkout receipt")){
+
+                    if(messages[i].isMimeType("multipart/*")){
+
+
+
+                        //brakes up email parts
+                        MimeMultipart mimeParts = (MimeMultipart) messages[i].getContent();
+
+
+                        //MultipartEmail test =new MultipartEmail(messages[i]);
+
+                        for(int j =0; j< mimeParts.getCount();j++){
+
+                            //decodes email
+                            if(mimeParts.getBodyPart(j).isMimeType("text/plain")){
+                                String s = (String) mimeParts.getBodyPart(j).getContent();
+                                String test[]=s.split("\n");
+
+                                ArrayList<String> bookInfo =new ArrayList<String>();
+
+                               //scrapes email for data on book and adds it to list of book info
+                               for (int k=0;k<test.length-1;k++){
+                                   if(test[k].contains("Title:")||test[k].contains("Barcode:")||test[k].contains("Due Date:")){
+
+                                       bookInfo.add(test[k]);
+
+                                       //System.out.println(test[k]);
+                                   }
+                               }
+                               this.bookAndInfo.add(bookInfo);
+
+                            }
+
+
+
+
+                        }
+                    }
+                }
+
+            }
 
         } catch (Exception e) {
             System.out.println("no connection");
         }
     }
 
+    public ArrayList<ArrayList<String>> getBookAndInfo() {
+        return bookAndInfo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,16 @@ public class Main {
         String password="Halo12344";
 
         EmailConnection test = new EmailConnection(emailuserName,password);
+        //hardcoded test not for final use
+
+        for(int i=0; i<test.getBookAndInfo().size();i++){
+
+
+            for(int j=0;j<test.getBookAndInfo().get(i).size();j++){
+                System.out.println(test.getBookAndInfo().get(i).get(j));
+
+            }
+        }
+
     }
 }
